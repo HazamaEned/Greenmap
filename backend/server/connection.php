56e4305765ca716f65a08999e4e95789d9d454d2
ened <benedictjuson614@gmail.com>
2026-09-18 19:17:21 +0800

    debugging

--- backend/server/connection.php
+++ backend/server/connection.php
@@ -5,15 +5,16 @@ ini_set('display_errors', 1);
 require_once __DIR__ . '/../../env_loader.php';
 loadEnv(__DIR__ . '/../../.env');
 
-var_dump($host, $port, $user, $pass, $dbname, $sslCa);
-die();
-
 $host    = getenv('DB_HOST');
 $port    = (int) getenv('DB_PORT');
 $user    = getenv('DB_USER');
 $pass    = getenv('DB_PASS');
 $dbname  = getenv('DB_NAME');
 $sslCa   = getenv('DB_SSL_CA');
+
+var_dump($host, $port, $user, $pass, $dbname, $sslCa);
+die();
+
 
 $conn = mysqli_init();
 
